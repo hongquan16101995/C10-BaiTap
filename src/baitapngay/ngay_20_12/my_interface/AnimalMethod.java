f94e640f56a60b57a6bb391b53ad6df002e46664
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2021-12-23 09:22:03 +0700

    refactor các bài tập về 1 git

---
+++ src/baitapngay/ngay_20_12/my_interface/AnimalMethod.java
@@ -0,0 +1,7 @@
+package baitapngay.ngay_20_12.my_interface;
+
+public interface AnimalMethod {
+    int run();
+
+    String makeSound();
+}
---
+++ src/baitapthem/baitaptonghop/Dog.java
@@ -0,0 +1,8 @@
+package baitapthem.baitaptonghop;
+
+public class Dog extends MyAnimal {
+    @Override
+    public void run() {
+
+    }
+}
